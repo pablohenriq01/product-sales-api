a0545f0c71a6a07c625485bbe1c5c199b7c820ab
PabloH <pablohenriqq01@gmail.com>
2024-03-08 20:10:54 -0300

    subindo base do projeto

---
+++ src/main/java/github/com/frapodev/productsales/ProductSalesApplication.java
@@ -0,0 +1,13 @@
+package github.com.frapodev.productsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/frapodev/productsales/ProductSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.frapodev.productsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
